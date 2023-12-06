63a1c859c48328e53cee68a3ce0a9264c15c007e
Sammychesire <sammy.chesire@student.moringaschool.com>
2023-12-07 00:11:05 +0300

    initial contact summary templates

--- contact-summary.templated.js
+++ contact-summary.templated.js
@@ -8,6 +8,8 @@ const allReports = reports;
 let allFields = [];
 let allCards = [];
 
+console.log(thisContact);
+
 
 const householdMemberFields = [
   {appliesToType: ['household_member', 'household_contact'], label: 'Name', value: thisContact.name || thisContact.display_name, width: 4 },
@@ -19,6 +21,37 @@ const householdMemberFields = [
 const houseHoldFields = [
   {appliesToType: ['household'], label: 'Household Head', value: titleCaseLetters(thisContact.contact && thisContact.contact.name), width: 6 },
   {appliesToType: ['household'], label: 'Belongs To', appliesIf: () => thisContact.parent && thisLineage[0], value: thisLineage, filter: 'lineage', width: 6},
+];
+const facilityFields = [
+  {appliesToType: ['area_health_facility'], label: 'Facility Name', value: titleCaseLetters(thisContact.manual_name || thisContact.meta.name),width: 6},
+  {appliesToType: ['area_health_facility'], label: 'Belongs To',  value: 'MOH Kenya', width: 6},
+];
+const diseaseSurveillanceFields = [
+  {appliesToType: ['area_health_facility_nurse'], label: 'Name', value: thisContact.name || thisContact.meta.name, width: 4 },
+  {appliesToType: ['area_health_facility_nurse'], label: 'Phone', value: thisContact.phone||'Not Provided', width: 4},
+  {appliesToType: ['area_health_facility_nurse'], label: 'Belongs To', appliesIf: () => thisContact.parent && thisLineage[0], value: thisLineage, filter: 'lineage', width: 8 },
+];
+const communityHealthUnitFields = [
+  {appliesToType: ['community_health_area'], label: 'Community Health Unit', value: thisContact.name || thisContact.meta.name, width: 4 },
+  {appliesToType: ['community_health_area'], label: 'County', value: titleCaseLetters(thisContact.selected_county || thisContact.parent.selected_county), width: 4},
+  {appliesToType: ['community_health_area'], label: 'Sub-County', value: titleCaseLetters(thisContact.selected_sub_county || thisContact.parent.selected_sub_county), width: 4},
+  {appliesToType: ['community_health_area'], label: 'Ward', value: titleCaseLetters(thisContact.selected_ward || thisContact.parent.selected_ward), width: 4},
+  {appliesToType: ['community_health_area'], label: 'Belongs To', appliesIf: () => thisContact.parent && thisLineage[0], value: thisLineage, filter: 'lineage', width: 8 },
+];
+const chpFields = [
+  {appliesToType: ['community_health_volunteer'], label: 'Name', value: thisContact.name || thisContact.meta.name, width: 4 },
+  {appliesToType: ['community_health_volunteer'], label: 'Phone', value: thisContact.phone||'Not Provided', width: 4},
+  {appliesToType: ['community_health_volunteer'], label: 'Belongs To', appliesIf: () => thisContact.parent && thisLineage[0], value: thisLineage, filter: 'lineage', width: 8 },
+];
+const supervisorRegionFields = [
+  {appliesToType: ['area_supervisor_region'], label: 'Name', value: thisContact.name || thisContact.meta.name, width: 4 },
+  {appliesToType: ['area_supervisor_region'], label: 'Additional notes', value: thisContact.notes||'Not Available', width: 4},
+  {appliesToType: ['area_supervisor_region'], label: 'Belongs To', appliesIf: () => thisContact.parent && thisLineage[0], value: thisLineage, filter: 'lineage', width: 8 },
+];
+const chaFields = [
+  {appliesToType: ['area_community_health_supervisor'], label: 'Name', value: thisContact.name || thisContact.meta.name, width: 4 },
+  {appliesToType: ['area_community_health_supervisor'], label: 'Phone', value: thisContact.phone||'Not Provided', width: 4},
+  {appliesToType: ['area_community_health_supervisor'], label: 'Belongs To', appliesIf: () => thisContact.parent && thisLineage[0], value: thisLineage, filter: 'lineage', width: 8 },
 ];
 
 const householdMemberCards = [
@@ -61,6 +94,12 @@ const householdMemberCards = [
 
 allFields = pushFieldsToSingleArray(householdMemberFields, allFields);
 allFields = pushFieldsToSingleArray(houseHoldFields, allFields);
+allFields = pushFieldsToSingleArray(facilityFields, allFields);
+allFields = pushFieldsToSingleArray(diseaseSurveillanceFields, allFields);
+allFields = pushFieldsToSingleArray(communityHealthUnitFields, allFields);
+allFields = pushFieldsToSingleArray(chpFields, allFields);
+allFields = pushFieldsToSingleArray(chaFields, allFields);
+allFields = pushFieldsToSingleArray(supervisorRegionFields, allFields);
 allCards = pushFieldsToSingleArray(householdMemberCards, allCards);
 
 module.exports = {
