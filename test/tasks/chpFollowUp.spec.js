9e3906d8e2d77dd4a4cd36fdd55987a0c33fab74
Kembo <asininefatuity@gmail.com>
2024-02-01 00:15:17 +0300

    initial tasks and form tests

--- test/tasks/chpFollowUp.spec.js
+++ test/tasks/chpFollowUp.spec.js
@@ -1,7 +1,7 @@
-const chai = import('chai')
-const expect = chai.expect;
+const Chai = import('chai');
+const expect = Chai.expect;
 const Harness = require('cht-conf-test-harness');
-const harness = new Harness({verbose: true});
+const harness = new Harness({verbose: true, headless: false});
 
 describe('CHP Follow Up Tasks', () => {
 
@@ -11,20 +11,27 @@ beforeEach(async() => { return await harness.clear()});
 afterEach(async() => { expect(harness.consoleErrors).to.be.empty;});
 
 it('should create follow up task for household member', async() =>{
-    let report = {
-        form: 'household_member_assessment',
-        fields: {
-            initial_symptoms: 'yes'
-        },
-        reported_date: new Date().getTime()
-    };
-    let contact = {
-        contact_type: 'community_health_volunteer'
-    };
-    let tasks = harness.getTasks(contact,report);
+    const initialResult = await harness.fillForm('household_member_assessment', {
+        initial_symptoms: 'yes'
+    });
+    expect(initialResult.errors).to.be.empty;
+    let tasks = harness.getTasks();
     expect(tasks.length).toBe(1);
     expect(tasks[0].name).toBe('follow-up-household-member');
     expect(tasks[0].actions[0].form).toBe('cholera_suspicion_follow_up');
     expect(tasks[0].events[0].dueDate).toBeDefined();
 });
 });
+
+describe('Getting started tests', () => {
+    before(async () => { return await harness.start(); });
+    after(async () => { return await harness.stop(); });
+    beforeEach(async () => { return await harness.clear(); });
+    afterEach(() => { expect(harness.consoleErrors).to.be.empty; });
+  
+    const formName = 'household_member_assessment';
+    it(`${formName} can be loaded`, async () => {
+      await harness.loadForm(`${formName}`);
+      expect(harness.state.pageContent).to.include(`data-form-id="${formName}"`);
+    });
+  });
