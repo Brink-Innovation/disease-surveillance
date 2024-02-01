df9a457b98752db6c2c1435cbb8cb8330a2f450e
Kembo <asininefatuity@gmail.com>
2024-02-01 23:24:36 +0300

    wrote more script commands to test various components easily

--- test/tasks/chp_tasks.spec.js
+++ test/tasks/chp_tasks.spec.js
@@ -1,24 +1,23 @@
-const Chai = import('chai');
-const expect = Chai.expect;
+const { expect } = require('chai');
 const Harness = require('cht-conf-test-harness');
 const harness = new Harness({verbose: true, headless: true});
+const {houseHoldAssessmentScenarios} = require('../forms/form_inputs')
 
-// describe('CHP Follow Up Tasks', () => {
+describe('CHP Follow Up Tasks', () => {
 
-// before (async() => { return await harness.start()});
-// after (async() => { return await harness.stop()});
-// beforeEach(async() => { return await harness.clear()});
-// afterEach(async() => { expect(harness.consoleErrors).to.be.empty;});
+before (async() => { return await harness.start()});
+after (async() => { return await harness.stop()});
+beforeEach(async() => { return await harness.clear()});
+afterEach(async() => { expect(harness.consoleErrors).to.be.empty;});
 
-// it('should create follow up task for household member', async() =>{
-//     const initialResult = await harness.fillForm('household_member_assessment', {
-//         initial_symptoms: 'yes'
-//     });
-//     expect(initialResult.errors).to.be.empty;
-//     let tasks = harness.getTasks();
-//     expect(tasks.length).toBe(1);
-//     expect(tasks[0].name).toBe('follow-up-household-member');
-//     expect(tasks[0].actions[0].form).toBe('cholera_suspicion_follow_up');
-//     expect(tasks[0].events[0].dueDate).toBeDefined();
-// });
-// });
+it('should create follow up task for household member with case definition', async() =>{
+    const initialResult = await harness.fillForm('household_member_assessment', [...houseHoldAssessmentScenarios.choleraCaseDefinition]);
+    expect(initialResult.errors).to.be.empty;
+    let tasks = harness.getTasks();
+    console.log(tasks);
+    // expect(tasks.length).toBe(1);
+    // expect(tasks[0].name).toBe('follow-up-household-member');
+    // expect(tasks[0].actions[0].form).toBe('cholera_suspicion_follow_up');
+    // expect(tasks[0].events[0].dueDate).toBeDefined();
+});
+});
